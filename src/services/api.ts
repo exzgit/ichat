a3e33fecce7bc318ab6c605d11abbd3b43aedc32
ezra <officialbangezz@gmail.com>
2026-07-05 10:57:25 +0700

    fix the authentication and session areas so you can auto log in if the access token is still valid

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,16 @@ import * as sdk from "matrix-js-sdk";
 
 let __client__ = null;
 
-export const initClient = () => {
+export const initClient = (access_token?, device_id?, user_id?) => {
   if (!__client__)
-    __client__ = sdk.createClient({ baseUrl: "https://matrix.org" });
+    __client__ = sdk.createClient({
+      baseUrl: "https://matrix.org",
+      accessToken: access_token || null,
+      deviceId: device_id || null,
+      userId: user_id || null,
+    });
+
+  return __client__;
 };
 
 export const Client = () => {
